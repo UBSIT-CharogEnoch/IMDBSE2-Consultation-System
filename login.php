809946bff016dcc9bb6a3cf7b70578c1ce712e9f
Enoch <79896407+UBSIT-CharogEnoch@users.noreply.github.com>
2022-10-13 22:29:55 +0800

    Co-authored-by: Patrick <UBSITGarvillesPatrick@users.noreply.github.com>

---
+++ login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+
+    <style>
+        body{
+            padding: 0;
+            margin:0;
+            height: 100vh;
+            width: 100vw;
+            background: black;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .login-container{
+            height:20%;
+            width:20%;
+            background-color: white;
+        }
+
+
+    </style>
+</head>
+<body>
+    <div class="login-container">
+
+    </div>
+</body>
+</html>
